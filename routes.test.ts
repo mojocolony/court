262bca9749b1614d9ffcc8813a357dbf1865cc78
mojocolony <iamwilwal@gmail.com>
2026-09-08 06:03:37 -0400

    Delete routes.test.ts

--- routes.test.ts
+++
@@ -1,7 +0,0 @@
-import { describe, expect, it } from "vitest";
-import { parseRoute } from "../../src/app/routes";
-
-describe("parseRoute", () => {
-  it("defaults to Today", () => expect(parseRoute("")).toEqual({ name: "today" }));
-  it("parses a player route", () => expect(parseRoute("#/player/p123")).toEqual({ name: "player", id: "p123" }));
-});
